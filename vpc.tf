abdb13dfd0da0e7c8c890b1a385fe92153224c85
suciloknam <146013241+suciloknam@users.noreply.github.com>
2023-12-12 22:48:20 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -13,21 +13,21 @@ provider "aws" {
 
 # Create a VPC
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16" # Change this to your desired CIDR block for the VPC
+  cidr_block = "172.16.0.0/16" # Change this to your desired CIDR block for the VPC
 
   tags = {
-    Name = "MyVPC"
+    Name = "HOVPC"
   }
 }
 
 # Create a subnet within the VPC
 resource "aws_subnet" "my_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block             = "10.0.1.0/24" # Change this to your desired CIDR block for the subnet
+  cidr_block             = "172.16.10.0/24" # Change this to your desired CIDR block for the subnet
   availability_zone       = "us-east-1a" # Change this to your desired availability zone
   map_public_ip_on_launch = true # Set this to true if you want instances in this subnet to receive public IP addresses
 
   tags = {
-    Name = "MySubnet"
+    Name = "WorkSubnet"
   }
 }
